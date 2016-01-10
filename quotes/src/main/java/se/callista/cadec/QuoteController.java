9f5a10e4e1c7f3fe7e39bfe7a034f39768b70c06
Bjorn Beskow <bb@callistaenterprise.se>
2016-01-10 16:13:55 +0100

    Revert "v2"
    This reverts commit f6b3da6a8ceff804cae4e10c91d113180f876369.

--- quotes/src/main/java/se/callista/cadec/QuoteController.java
+++ quotes/src/main/java/se/callista/cadec/QuoteController.java
@@ -1,9 +1,7 @@
 package se.callista.cadec;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,19 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuoteController {
 
-	Map<String, List<String>> quotes;
+	List<String> quotes;
 	Random random = new Random();
 	
 	public QuoteController() {
-		quotes = new HashMap<>();
-		quotes.put("en", Arrays.<String>asList("To be or not to be", "You, too, Brutus?", "Champagne should be cold, dry and free"));
-		quotes.put("sv", Arrays.<String>asList("Att vara eller inte vara", "Även du, min Brutus?", "Champagne skall vara kall, torr och gratis"));
+		quotes = Arrays.<String>asList("To be or not to be", "You, too, Brutus?", "Champagne should be cold, dry and free");
 	}
 	
     @RequestMapping("/quote")
     public Quote quote(@RequestParam(value="language", defaultValue="en") String language) {
-		List<String> list = quotes.get(language);
-		String s = list.get(random.nextInt(list.size()));
+		String s = quotes.get(random.nextInt(quotes.size()));
 		Quote quote = new Quote();
 		quote.setQuote(s);
 		quote.setLanguage(language);

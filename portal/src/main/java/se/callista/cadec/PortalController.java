091912141238e9e66fd7c70bbe34f8a0a96afefe
Bjorn Beskow <bb@callistaenterprise.se>
2016-01-15 11:28:15 +0100

    Retry implemented.

--- portal/src/main/java/se/callista/cadec/PortalController.java
+++ portal/src/main/java/se/callista/cadec/PortalController.java
@@ -2,15 +2,20 @@ package se.callista.cadec;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class PortalController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PortalController.class);
+	
 	@Value("${quote.server}")
 	private String quoteServer;
 	
@@ -22,7 +27,19 @@ public class PortalController {
     @RequestMapping("/home")
     public String home(Model model, Locale locale) {
     	String language = locale.getLanguage();
-        Quote quote = restTemplate.getForObject("http://"+quoteServer+":"+quotePort+"/quote?language="+language, Quote.class);
+    	Quote quote = null;
+    	int retries = 0;
+    	while (quote == null && retries < 5) {
+	        try {
+				quote = restTemplate.getForObject("http://"+quoteServer+":"+quotePort+"/quote?language="+language, Quote.class);
+			} catch (RestClientException e) {
+				LOG.warn("Failed to get quote: " + e.getMessage());
+				retries++;
+			}
+		}
+    	if (quote == null) {
+    		throw new RuntimeException("Cannot get quote");
+    	}
     	model.addAttribute("quote", quote.getQuote());
         return "home";
     }
